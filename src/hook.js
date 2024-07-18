4dbccaacea0879ee3743577a76a21379fd835dba
dalramjwi <dalramjwi@gmail.com>
2024-07-18 18:10:18 +0900

    useEffect js VERSION 작성

--- src/hook.js
+++ src/hook.js
@@ -9,3 +9,16 @@ const setState = (newState) => {
 };
 // setState 함수 호출, state 값을 1로 업데이트
 setState(1); // 상태 업데이트
+//! useEffect in JS
+
+//Vanilla JS 부수효과 관리
+// document에 클릭 이벤트 리스너 추가
+document.addEventListener("click", () => {
+  // 클릭 시 콘솔에 메시지 출력
+  console.log("Document clicked");
+});
+
+// Cleanup function: 이벤트 리스너 제거
+document.removeEventListener("click", () => {
+  console.log("Document clicked");
+});
