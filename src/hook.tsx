46d3ba0d1532b68e372c46620cdd638de00cf885
dalramjwi <dalramjwi@gmail.com>
2024-07-18 18:29:47 +0900

    useEFfect react VERSION 작성

--- src/hook.tsx
+++ src/hook.tsx
@@ -1,5 +1,5 @@
 // React 라이브러리에서 useState Hook 가져오기
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Counter 컴포넌트 정의
 function Counter() {
@@ -20,4 +20,33 @@ function Counter() {
   );
 }
 
-export default Counter; // Counter 컴포넌트를 내보내기
+// React 라이브러리에서 useEffect Hook 가져오기
+
+// ClickLogger 컴포넌트 정의
+function ClickLogger() {
+  // useEffect Hook 사용
+  /**
+   * 매번 컴포넌트가 렌더링 될 때 특정 조건에 의존하여 수행되며, 컴포넌트가 최대한 순수 함수를 유지할 수 있도록 도와주는 함수
+   * 이벤트 핸들러에 속하는 개념
+   * 지금까지 사용한 이벤트 핸들러는 렌더링 이후, 사용자의 상호작용에 의해 실행되는 함수
+   *useEffect는 렌더링 이후에 실행되고, 특정 조건을 만족하면 실행된다는 차이가 존재
+   * useEffect가 실행되는 시점은 컴포넌트가 렌더링된 이후
+   * !useEffect(function, deps)
+   * function 실행하고자 하는 함수
+   * deps 배열 형태, function을 실행시키는 조건. deps에 특정 값을 넣으면 컴포넌트가 나타날 때 혹은 지정한 값이 업데이트 될 때 useEffect 실행
+   **/
+  useEffect(() => {
+    // 클릭 핸들러 함수 정의
+    const handleClick = () => console.log("Document clicked");
+
+    // 컴포넌트가 마운트될 때 document에 클릭 이벤트 리스너 추가
+    document.addEventListener("click", handleClick);
+
+    // 클린업 함수 반환: 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []); // 의존성 배열이 빈 배열이므로, 한 번만 실행됨
+
+  return <div>Click anywhere to log to console.</div>; // JSX 반환
+}
